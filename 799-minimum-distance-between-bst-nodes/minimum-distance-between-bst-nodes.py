bd66284673250fd0f74d4a84dfbe34d3b627115a
lakshmandhulipudi <149046466+lakshmandhulipudi@users.noreply.github.com>
2026-03-10 18:09:59 +0530

    Time: 4 ms (4.39%) | Memory: 19.3 MB (72.28%) - LeetSync

--- 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.py
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.py
@@ -4,21 +4,41 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def inorder(self,root,res):
-            if root==None:
-                return
-            self.inorder(root.left,res)
-            res.append(root.val)
-            self.inorder(root.right,res)
+
+#BRUTE FORCE
+# class Solution:
+#     def inorder(self,root,res):
+#             if root==None:
+#                 return
+#             self.inorder(root.left,res)
+#             res.append(root.val)
+#             self.inorder(root.right,res)
         
+#     def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+#         res=[]
+#         self.inorder(root,res)
+#         mini = float('inf')
+#         for i in range(1,len(res)):
+#             diff = res[i] - res[i-1]
+#             if diff < mini:
+#                 mini = diff
+#         return mini
+
+
+class Solution:
     def minDiffInBST(self, root: Optional[TreeNode]) -> int:
-        res=[]
-        self.inorder(root,res)
-        mini = float('inf')
-        for i in range(1,len(res)):
-            diff = res[i] - res[i-1]
-            if diff < mini:
-                mini = diff
-        return mini
+
+        self.prev = None
+        self.mini=float('inf')
+        def helper(root):
+            if root==None:
+                return 
+            helper(root.left)
+            if self.prev!=None:
+                self.mini=min(self.mini,root.val-self.prev)
+            self.prev=root.val
+            helper(root.right)
+        helper(root)
+        return self.mini
+
         
