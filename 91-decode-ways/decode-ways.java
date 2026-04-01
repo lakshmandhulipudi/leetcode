98964f9698b2f7ea05ffb3bc96528561c95eab41
lakshmandhulipudi <149046466+lakshmandhulipudi@users.noreply.github.com>
2026-04-01 17:58:46 +0530

    Time: 1 ms (85.69%) | Memory: 43 MB (73.31%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        if(n==0||s.charAt(0)=='0') return 0;
+        if(n==1) return 1;
+        int dp[] = new int[n+1];
+        dp[0] = dp[1] = 1;
+        for(int i = 2; i<= n;i++){
+            char c = s.charAt(i-1),c1=s.charAt(i-2);
+            if(c > '0') dp[i] = dp[i-1];
+            if(c1=='1' ||(c1=='2'&&c<='6'))dp[i]+=dp[i-2];
+        }
+        return dp[n];
+        
+    }
+}
